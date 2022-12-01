1b78c8cd81d18a3e2979c3aa6358b613dab03579
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-12-01 14:35:11 +0100

    add Card logic

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,32 @@ import { useState } from "react";
 import styled from "styled-components";
 import Card from "../components/Card/Card";
 import Form from "../components/Form/Form";
+import { nanoid } from "nanoid";
 
 export default function HomePage() {
   const [cards, setCards] = useState([]);
+  console.log(cards);
+
   function handleSubmit(event) {
     event.preventDefault();
     const thought = event.target.elements.thoughts.value;
     const owner = event.target.elements.owner.value;
     event.target.reset();
-    event.target.owner.focus();
-    setCards([...cards, { thought: thought, owner: owner }]);
+
+    setCards([...cards, { thought: thought, owner: owner, id: nanoid() }]);
   }
 
   return (
     <>
       <StyledHeader>Lean Coffee Board</StyledHeader>
       <hr></hr>
-      <Card card={cards}></Card>
+      <StyledBoard>
+        {cards.map((card) => {
+          return (
+            <Card key={card.id} owner={card.owner} thought={card.thought} />
+          );
+        })}
+      </StyledBoard>
       <hr></hr>
       <Form onSubmit={handleSubmit} />
     </>
@@ -29,4 +38,10 @@ const StyledHeader = styled.h1`
   font-family: monospace;
   text-align: center;
   text-size-adjust: 124px;
+`;
+
+const StyledBoard = styled.ul`
+  display: flex;
+  justify-content: row;
+  gap: 10px;
 `;
